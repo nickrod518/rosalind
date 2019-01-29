52e5deccdd414a422612b49927ff1912f6534ef9
Rodriguez <Nick.Rodriguez@dhg.com>
2019-01-29 16:31:42 -0500

    ini solution

---
+++ ini.py
@@ -0,0 +1,18 @@
+#todo use Bio.Seq instead
+
+a_count = 0
+c_count = 0
+g_count = 0
+t_count = 0
+
+for c in input():
+    if c == "A":
+        a_count = a_count + 1
+    elif c == "C":
+        c_count = c_count + 1
+    elif c == "G":
+        g_count = g_count + 1
+    elif c == "T":
+        t_count = t_count + 1
+
+print(a_count, c_count, g_count, t_count)
